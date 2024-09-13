efadf22656ebcd1e5351a493ddc07d656f02818e
MohamedKhalid <Mohamed.khaled1872002@gmail.com>
2024-09-13 23:25:57 +0300

    solve third  exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    const splittedString = originalString.split("");
+    
+    const reversedArray = splittedString.reverse();
 
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
