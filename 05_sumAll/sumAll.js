0ebcda90754e330fd7a8388906892f16345e10ab
MohamedKhalid <Mohamed.khaled1872002@gmail.com>
2024-09-14 00:05:47 +0300

    solve fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    const smaller = Math.min(num1, num2) - 1;
+    const larger = Math.max(num1, num2);
 
+    return Math.abs(smaller*(smaller + 1)/2 - larger*(larger + 1)/2);
 };
 
 // Do not edit below this line
