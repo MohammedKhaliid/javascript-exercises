9c13e9347ac6c769483f0a747a380cbd95e427b9
MohamedKhalid <Mohamed.khaled1872002@gmail.com>
2024-09-14 00:16:21 +0300

    solve sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(!Number.isInteger(year) || year < 0) return "ERROR";
+
+    if(year%400 == 0 || year%4==0 && year%100) return true;
+    return false;
 
 };
 
