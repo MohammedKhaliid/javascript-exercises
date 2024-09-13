a01ca4120a6a6dac561929382303cf4debac73ca
MohamedKhalid <Mohamed.khaled1872002@gmail.com>
2024-09-13 23:54:00 +0300

    solve forth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...rmv) {
+
+    for(let i = 0 ; i < rmv.length; i++){
+        while(true){
+            const index = originalArray.indexOf(rmv[i]);
+            
+            if(index == -1) break;
+            
+            originalArray.splice(index, 1);
+        }
+    }
+
+    return originalArray;
+
+    //Look at the solutions (first one)
 };
 
 // Do not edit below this line
