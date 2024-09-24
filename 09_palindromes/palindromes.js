ed03d96f7b7b1e010b4ac693659c3eb37f907ef6
MohamedKhalid <Mohamed.khaled1872002@gmail.com>
2024-09-24 22:42:44 +0300

    Solve ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.split('').filter((element)=>
+    (element.toLowerCase()>='a' 
+    && element.toLowerCase() <= 'z'
+    || element >= '1' && element <= '9' )).join();
 
+    for(let i = 0 ;i < str.length/2; i++){
+        if(str[i].toLowerCase() != str[str.length - i - 1].toLowerCase()) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
