73ea3d6efbdc674bdc343d03c81b8bb4a065bd8f
MohamedKhalid <Mohamed.khaled1872002@gmail.com>
2024-09-24 23:14:40 +0300

    Solve last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    people.sort((personA, personB) =>
+         ((personA.yearOfDeath? personA.yearOfDeath: new Date().getFullYear()) - personA.yearOfBirth)
+         -((personB.yearOfDeath? personB.yearOfDeath: new Date().getFullYear())- personB.yearOfBirth)
+         );
+
+    return people.at(-1);
 };
 
 // Do not edit below this line
